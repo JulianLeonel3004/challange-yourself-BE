d18e3c03a7d78598e3c1abc0eaaf7fde1ec844ea
Julian <julian.leonel3004@gmaio.com>
2022-02-13 17:42:02 -0300

    install libraries

---
+++ src/controllers/userController.js
@@ -0,0 +1,13 @@
+const userController = {
+    register : req => fun(req)
+
+}
+
+module.exports = userController
+
+
+
+const fun = req => {
+    console.log(req)
+    return "hola"
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,16 @@
-
-
+const userController = require('../controllers/userController')
 
 const appRouter = app => {
 
+    const bodyParser = require('body-parser');
+    app.use(bodyParser.json()); // support json encoded bodies
+    app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+ 
+    
+  app.post("/register", async (req, res) => {
+    const resp = await userController.register(req.body)
+    res.status(200).send(resp)
+  })
  
 }
 
